387d51721abeab3aacb00ae9d3949818432c233b
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2021-03-12 13:33:42 +0700

    first commit

---
+++ src/Containers/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import { Heroes } from './Heroes';
+import logo from './MLBBLogo.svg';
+import './app.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            Heroes: Heroes,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value})
+    }  
+
+    render() {
+        const filteredHeroes = this.state.Heroes.filter(Heroes =>{
+            return Heroes.heroname.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return (
+            <div className='font-mono text-center'>
+                <header className='pt-2 h-1/3 flex flex-col items-center justify-center'>
+                    <img src={logo} className='w-1/5 al' alt="MLBBlogo"/>
+                    <h1 className='text-2xl text-gray-200 font-bold'>Fans Database</h1>
+                </header>
+                <SearchBox searchChange={this.onSearchChange} />
+                <div className='overflow-y-auto h-96'>
+                    <CardList Heroes={filteredHeroes} />
+                </div>
+                <footer>
+                    <h1 className='pt-4 text-gray-200'>Build by Alfian Nahar</h1>
+                </footer>
+            </div>     
+        );
+    }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <App />
+  , document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log)
+
+
+
+const urls = [
+  'https://swapi.dev/api/people/1',
+  'https://swapi.dev/api/people/2',
+  'https://swapi.dev/api/people/3',
+  'https://swapi.dev/api/people/4'
+];
+
+Promise.all(urls.map(url =>
+    fetch(url).then(people => people.json())
+))
+  .then(array => {
+    console.log('1', array[0])
+    console.log('2', array[1])
+    console.log('3', array[2])
+    console.log('4', array[3])
+  })
+  .catch(err => console.log('ughhhh fix it!', err));
